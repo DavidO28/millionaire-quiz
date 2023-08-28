f778cca3de2bd7ab1e1b33801ef34fab28de8087
DavidO28 <damers22@yahoo.com>
2023-08-28 21:54:02 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,311 @@
+import { useEffect, useState } from "react";
+import Trivia from "./Trivia";
+import Timer from "./Timer";
+
+function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
+  const [earned, setEarned] = useState();
+
+  const data = [{
+    id: 1,
+    question: "რომელი ფირმის ტელეფონს აღმერთებდა მირო ადრე, ახლა კი ეზიზღება იმის გამო რო თავისას აჯობა?",
+    answers: [
+      {
+        text: "სამსუნგს",
+        correct: false,
+      },
+      {
+        text: "პიქსელს",
+        correct: true,
+      },
+      {
+        text: "ქსიაომის",
+        correct: false,
+      },
+      {
+        text: "ჰუავეის",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "რომელი ჯობია?",
+    answers: [
+      {
+        text: "კრუზი",
+        correct: true,
+      },
+      {
+        text: "ჯეტა",
+        correct: false,
+      },
+      {
+        text: "ოპელ ელეგანტი",
+        correct: false,
+      },
+      {
+        text: "ბენვე",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "ვინ დაკარგეს 200$-300$ ბოტასსში ან/და ლისტში",
+    answers: [
+      {
+        text: "ზურამ და გიომ",
+        correct: false,
+      },
+      {
+        text: "ზურამ და დიტომ",
+        correct: false,
+      },
+      {
+        text: "დიტომ და გიომ",
+        correct: false,
+      },
+      {
+        text: "მირომ და დემნამ",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "რამდენი აქვს დემნას ხელფასი რეალურად?",
+    answers: [
+      {
+        text: "+ უსასრულობა",
+        correct: false,
+      },
+      {
+        text: "1100 მოჭრილი და რასაც გადაუგდებენ ან მოტეხავს (1500მდე)",
+        correct: true,
+      },
+      {
+        text: "შენზე მეტი",
+        correct: false,
+      },
+      {
+        text: "შენზე ორჯერ მეტი",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "როდის ანებებს დიტო სიგარეტს თავს?",
+    answers: [
+      {
+        text: "ეს ბოლო იყო უკვე",
+        correct: true,
+      },
+      {
+        text: "ხვალაც მოვწევ და ეგააა",
+        correct: false,
+      },
+      {
+        text: "მომავალი თვიდან",
+        correct: false,
+      },
+      {
+        text: "გუშინ",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "ვის ჰქონდა ნერვოზი 2022 წელს?",
+    answers: [
+      {
+        text: "დათოს",
+        correct: true,
+      },
+      {
+        text: "მიროს",
+        correct: false,
+      },
+      {
+        text: "დიტოს",
+        correct: false,
+      },
+      {
+        text: "გიოს",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "ვინ არ სცემს პატივს HTML, CSS, JS-ს? და ამავდროულად 'იცის' ქოუდინგი",
+    answers: [
+      {
+        text: "დათო",
+        correct: false,
+      },
+      {
+        text: "მირო",
+        correct: true,
+      },
+      {
+        text: "დიტო",
+        correct: false,
+      },
+      {
+        text: "დემნა",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "ვინ არის კაკლის საბერტყი ჯოხივით მაღალი?",
+    answers: [
+      {
+        text: "დათო",
+        correct: false,
+      },
+      {
+        text: "მირო",
+        correct: false,
+      },
+      {
+        text: "გიო",
+        correct: true,
+      },
+      {
+        text: "დიტო",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "რომელია ყველაზე მეტი?",
+    answers: [
+      {
+        text: "1",
+        correct: false,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "3",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "რომელია ყველაზე ნაკლები?",
+    answers: [
+      {
+        text: "1",
+        correct: true,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "3",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "შენ გაიმარჯვე",
+    answers: [
+      {
+        text: "შენ გაიმარჯვე",
+        correct: false,
+      },
+      {
+        text: "შენ გაიმარჯვე",
+        correct: false,
+      },
+      {
+        text: "შენ გაიმარჯვე",
+        correct: false,
+      },
+      {
+        text: "შენ გაიმარჯვე",
+        correct: false,
+      },
+    ],
+  },
+  ];
+
+  const moneyPyramid = [
+    { id: 1, amount: "₾ 2.000" },
+    { id: 2, amount: "₾ 4.000" },
+    { id: 3, amount: "₾ 8.000" },
+    { id: 4, amount: "₾ 16.000" },
+    { id: 5, amount: "₾ 32.000" },
+    { id: 6, amount: "₾ 64.000" },
+    { id: 7, amount: "₾ 125.000" },
+    { id: 8, amount: "₾ 250.000" },
+    { id: 9, amount: "₾ 500.000" },
+    { id: 10, amount: "₾ 1.000.000" }
+  ].reverse();
+
+
+  useEffect(() => {
+    questionNumber > 1 && setEarned(moneyPyramid.find(m => m.id === questionNumber - 1).amount);
+  }, [questionNumber]);
+
+  return (
+    <div className="App">
+      <div className="main">
+        {timeOut ? (<h1 className="endText">პასუხი მცდარია, შენ წააგე {earned}</h1>) :
+          <>
+            <div className="top">
+              <div className="timer">
+                <Timer
+                  setTimeOut={setTimeOut}
+                  questionNumber={questionNumber}
+                />
+              </div>
+            </div>
+            <div className="bottom">
+              <Trivia
+                data={data}
+                setTimeOut={setTimeOut}
+                questionNumber={questionNumber}
+                setQuestionNumber={setQuestionNumber}
+              />
+            </div>
+          </>
+        }
+
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid.map(m => (
+            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"} >
+              <span className="moneyListItemNumber">{m.id} შეკითხვა</span>
+              <span className="moneyListItemAmount">- {m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
